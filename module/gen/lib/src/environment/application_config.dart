3af02b78bb97b1f3d09acc7f09f126c806c79560
gokhanvaris <gokhanvaris@outlook.com.tr>
2023-11-06 20:36:01 +0300

    project scripts added

---
+++ module/gen/lib/src/environment/application_config.dart
@@ -0,0 +1,8 @@
+/// The above class is an abstract class for app configuration.
+abstract class ApplicationConfig {
+  /// baseUrl for network manager
+  String get baseUrl;
+
+  /// apiKey for requests
+  String get apiKey;
+}
---
+++ module/gen/lib/src/index.dart
@@ -0,0 +1,2 @@
+export 'asset/index.dart';
+export 'environment/index.dart';
